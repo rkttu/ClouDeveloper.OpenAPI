1e3aca861b80f1e8eff442a68ae0adef0ff442f0
rkttu <rkttu@xinics.com>
2015-05-08 15:08:06 +0900

    modify buildpackages.cmd to push each release package.

--- BuildPackages.cmd
+++ BuildPackages.cmd
@@ -6,14 +6,16 @@ if not exist RedistPackages\libs mkdir RedistPackages\libs
 
 set projectname=ClouDeveloper.OpenAPI.KolisNet
 .nuget\nuget.exe pack %projectname%\%projectname%.csproj -Build -Symbols -IncludeReferencedProjects -OutputDirectory "RedistPackages\libs" -Properties configuration=Release
+.nuget\NuGet.exe push RedistPackages\libs\%projectname%.*.nupkg
 
 set projectname=ClouDeveloper.OpenAPI.TED
 .nuget\nuget.exe pack %projectname%\%projectname%.csproj -Build -Symbols -IncludeReferencedProjects -OutputDirectory "RedistPackages\libs" -Properties configuration=Release
+.nuget\NuGet.exe push RedistPackages\libs\%projectname%.*.nupkg
 
 set projectname=ClouDeveloper.OpenAPI.Naver
 .nuget\nuget.exe pack %projectname%\%projectname%.csproj -Build -Symbols -IncludeReferencedProjects -OutputDirectory "RedistPackages\libs" -Properties configuration=Release
+.nuget\NuGet.exe push RedistPackages\libs\%projectname%.*.nupkg
 
-.nuget\NuGet.exe push RedistPackages\libs\ClouDeveloper.OpenAPI.*.nupkg
 start RedistPackages
 
 popd
